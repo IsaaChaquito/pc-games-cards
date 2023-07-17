8902c03aea93e855fa5b41581ff9d8076d5786ba
Isaac Castro G <87621852+IsaaChaquito@users.noreply.github.com>
2023-07-16 22:46:03 -0600

    some css with style B-)

--- about-games/src/app/page.jsx
+++ about-games/src/app/page.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-'use client'
+"use client";
 
 // export const dynamic = 'force-dynamic'
 // export const dynamicParams = true
@@ -8,91 +8,110 @@
 // export const runtime = 'nodejs'
 // export const preferredRegion = 'auto'
 
-import React, { useEffect, useState } from 'react'
-import './assets/fonts/fonts.css'
-import { getGamesPerPage } from './services/api-games'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-import '@fortawesome/fontawesome-svg-core/styles.css'
+import React, { useEffect, useState } from "react";
+import "./assets/fonts/fonts.css";
+import { getGamesPerPage } from "./services/api-games";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 export default function Home() {
+	const [games, setGames] = useState();
 
-  const [games, setGames] = useState()
+	const getFirstPageGames = async () => {
+		await getGamesPerPage(1).then((res) => {
+			console.log("res home", res);
+			setGames(res);
+		});
+	};
 
-  const getFirstPageGames = async () =>{
-    await getGamesPerPage(1)
-    .then(res =>{
-      console.log('res home', res)
-      setGames(res)
-    })
-  }
+	useEffect(() => {
+		!games && getFirstPageGames();
+	}, [games]);
 
-  useEffect(() => {
-    !games && getFirstPageGames()
-  }, [games])
-  
-    const rating = [
-      '⭐',
-      '⭐⭐',
-      '⭐⭐⭐',
-      '⭐⭐⭐⭐',
-      '⭐⭐⭐⭐⭐',
-    ]
+	const rating = ["⭐", "⭐⭐", "⭐⭐⭐", "⭐⭐⭐⭐", "⭐⭐⭐⭐⭐"];
 
-    const colorRating = [//background color depending the game rating
-      'one-star',
-      'tow-stars',
-      'three-stars',
-      'four-stars',
-      'five-stars',
-    ]
+	const colorRating = [
+		//background color depending the game rating
+		"one-star",
+		"tow-stars",
+		"three-stars",
+		"four-stars",
+		"five-stars",
+	];
 
-  const titleStyle = "text-rose-600 the-boys-font select-none leading-normal hover:drop-shadow-2xl mr-5 title-home shadowed drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] pl-2 pr-2 "
+	const titleStyle =
+		"text-rose-600 the-boys-font select-none leading-normal hover:drop-shadow-2xl mr-5 title-home shadowed drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] pl-2 pr-2 ";
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='flex flex-col md:flex-row'>
+	const spanDetailsStyle =
+		"rating flex absolute left-[-100%] group-hover:left-1 duration-300 ";
 
-        <span className={titleStyle}>is</span>
-        <span className={titleStyle + 'spacing-effect'}>this</span>
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<div className="flex flex-col md:flex-row">
+				<span className={titleStyle}>is</span>
+				<span className={titleStyle + "spacing-effect"}>this</span>
 
-        <div className='flex flex-col'>
-          <span className={titleStyle + 'flip-1'}>Entertaining?</span>
-          <span className={titleStyle + 'flip-2'}>Boring?</span>
-        </div>
-      </div>
+				<div className="flex flex-col">
+					<span className={titleStyle + "flip-1"}>Entertaining?</span>
+					<span className={titleStyle + "flip-2"}>Boring?</span>
+				</div>
+			</div>
 
-      <section className='section flex flex-wrap justify-evenly mt-5 gap-8 the-boys-font'>
-        {games && games.results.map((game) =>{ 
-          return(
-            <div key={game.id} className='card group overflow-hidden border-spacing-3 flex flex-col text-[.8rem] text-gray-950 bg-[#ffffffd3] rounded-sm '>
+			<section className="section flex flex-wrap justify-evenly mt-40 gap-8 the-boys-font">
+				{games &&
+					games.results.map((game) => {
+						return (
+							<div className="card-wrapper relative">
+									<span className="Update">
+										<span className={colorRating[Math.floor(game.rating)]}>{rating[Math.floor(game.rating) - 1]}</span>
+									</span>
+								<div
+									key={game.id}
+									className="card group overflow-hidden border-spacing-3 flex flex-col text-[.8rem] text-gray-950 bg-[#ffffffd3] rounded-sm"
+								>
 
+									<img
+										className="card-background duration-300"
+										src={game.background_image}
+										alt="Image"
+									></img>
 
-              <img className='card-background ' src={game.background_image} alt='Image'></img>
+									<span
+										className={`absolute top-0 w-full h-[100%] group-hover:h-14 flex items-center justify-center text-xl text-center  pl-1 pr-1 text-[#ffffffd3] bg-[#00000081] group-hover:bg-[#000000] group-hover:top-[57%] duration-300`}
+									>
+										{game.name}{" "}
+									</span>
 
-              <span className={`absolute top-0 w-full self-center text-xl text-center pl-1 pr-1 group-hover:bg-gray-950 group-hover:text-[#ffffffd3] bg-[#ffffffd3]  group-hover:top-[57%] duration-300 `}>{game.name} </span>
+									<span
+										className={spanDetailsStyle + "bottom-[3rem] delay-[100ms]"}
+									>
+										Rating:&nbsp;
+										{game.rating}
+									</span>
 
-              <div className='absolute bottom-[-100px] flex flex-col w-full pl-2 pr-2 z-10 group-hover:bottom-0 duration-300 '>
-                <div className='card-details flex flex-col mt-10'>
-                  <span className='rating flex'>
-                    {game.rating}
-                    {rating[Math.floor(game.rating)-1]}
-                  </span>
-                  <span>Gendre:
-                    {game.genres.map((genre) =>{
-                      return genre.name + ' '
-                    })}
-                  </span>
-                  <span>Released: {game.released}</span>
-                  <span>Price: $60</span>
-              </div>
+									<span
+										className={spanDetailsStyle + "bottom-[2rem] delay-[200ms]"}
+									>
+										Gendre:
+										{game.genres.map((genre) => {
+											return genre.name + " ";
+										})}
+									</span>
 
-              </div>
-            </div>
-          )
-          
-        })}
-      </section>
+									<span
+										className={spanDetailsStyle + "bottom-[1rem] delay-[300ms]"}
+									>
+										Released: {game.released}
+									</span>
 
-    </main>
-  )
+									<span className={spanDetailsStyle + "bottom-0 delay-[400ms]"}>
+										Price: $60
+									</span>
+								</div>
+							</div>
+						);
+					})}
+			</section>
+		</main>
+	);
 }
